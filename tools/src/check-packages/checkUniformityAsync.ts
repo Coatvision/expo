9b73a089f86ff7a37a27902a68d9090f5315c8cf
Kudo Chien <kudo@expo.dev>
2025-11-20 04:06:13 +0800

    [tools] fix et cp clrf on windows

--- tools/src/check-packages/checkUniformityAsync.ts
+++ tools/src/check-packages/checkUniformityAsync.ts
@@ -1,9 +1,11 @@
+import fs from 'fs';
+import os from 'os';
 import path from 'path';
 
 import { EXPO_DIR } from '../Constants';
 import logger from '../Logger';
 import { Package } from '../Packages';
-import { spawnAsync } from '../Utils';
+import { searchFilesAsync, spawnAsync } from '../Utils';
 
 /**
  * Checks whether the state of files is the same after running a script.
@@ -11,6 +13,23 @@ import { spawnAsync } from '../Utils';
  * @param match Path or pattern of the files to match
  */
 export default async function checkUniformityAsync(pkg: Package, match: string): Promise<void> {
+  if (os.platform() === 'win32' && (await isGitTrackedAsync(pkg, match))) {
+    // Transform new line in map files to ensure that the map files are consistent.
+    await transformNewLineInMapFilesAsync(pkg, match);
+
+    // On Windows, we touch the files to force git for the CRLF -> LF conversion.
+    // This is a workaround for the fact that Git on Windows does not automatically convert CRLF to LF.
+    // warning: in the working copy of 'build/test.d.ts', CRLF will be replaced by LF the next time Git touches it
+    await spawnAsync('git', ['add', '-u', match], {
+      stdio: 'ignore',
+      cwd: pkg.path,
+    });
+    await spawnAsync('git', ['reset', match], {
+      stdio: 'ignore',
+      cwd: pkg.path,
+    });
+  }
+
   const child = await spawnAsync('git', ['status', '--porcelain', match], {
     stdio: 'pipe',
     cwd: pkg.path,
@@ -25,5 +44,38 @@ export default async function checkUniformityAsync(pkg: Package, match: string):
     });
 
     throw new Error(`${pkg.packageName} has uncommitted changes after building.`);
+  }
+}
+
+async function transformNewLineInMapFilesAsync(pkg: Package, match: string): Promise<void> {
+  const mapFiles = Array.from(await searchFilesAsync(pkg.path, `${match}/**/*.js.map`));
+  await Promise.all(
+    mapFiles.map(async (mapFile) => {
+      const filePath = path.join(pkg.path, mapFile);
+      try {
+        const contents = await fs.promises.readFile(filePath, 'utf8');
+        const map = JSON.parse(contents);
+        if (map.sourcesContent) {
+          map.sourcesContent = map.sourcesContent.map((sourceContent: string) =>
+            sourceContent.replace(/\r\n/g, '\n')
+          );
+        }
+        const newContents = JSON.stringify(map);
+        await fs.promises.writeFile(filePath, newContents);
+      } catch (error: unknown) {
+        logger.warn(`Failed to transform new line in map file ${filePath}: ${error}`);
+      }
+    })
+  );
+}
+
+async function isGitTrackedAsync(pkg: Package, match: string): Promise<boolean> {
+  try {
+    const { stdout } = await spawnAsync('git', ['ls-files', match], {
+      cwd: pkg.path,
+    });
+    return stdout.trim() !== '';
+  } catch {
+    return false;
   }
 }
